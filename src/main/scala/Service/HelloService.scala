eb4fadec14c24d87ae71df03a41922fa3c44d50d
Priyanka <sainatha123>
2020-03-04 12:14:54 +0530

    updated

---
+++ src/main/scala/Service/HelloService.scala
@@ -0,0 +1,16 @@
+package service
+ 	 
+ 	object HelloService
+ 	{
+ 	def hello =
+ 	{
+ 	"Hello" // tested by unit test
+ 	}
+ 	 
+ 	def hello2 =
+ 	{
+ 	"Hello2" // tested by integration test
+ 	}
+ 	 
+ 	}
+
